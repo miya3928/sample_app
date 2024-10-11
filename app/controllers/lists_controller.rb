f16f587ee23fd563455e4ee9cf169187a012cc20
TatsuyaMiyagawa <181440097+miya3928@users.noreply.github.com>
2024-10-11 07:35:09 +0000

    [delete_error]3-7]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,29 +6,36 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to list_path(list.id) 
+    redirect_to list_path(list.id)
   end
 
   def index
-    @lists = List.all  
+    @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])  
+    @list = List.find(params[:id])
   end
 
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
   
+  def destroy
+   list = List.find(params[:id])
+   list.destroy
+   redirect_to lists_path
+  end
+
+
   private
-  
+
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
